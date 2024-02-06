cf1770559157a57c6fc62e4fe0ca9409221b5423
Edgar Martinez <edgar.martinez.tech@gmail.com>
2024-02-06 16:21:44 -0600

    Add header and footer components

---
+++ src/app/components/layout/Footer.tsx
@@ -0,0 +1,6 @@
+export const Footer = () => {
+    return (
+        <div className="flex w-[100%] h-[300px] bg-gray-200">
+        </div>
+    )
+}
---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <div className="flex justify-center items-center w-[100%] h-[100px] bg-gray-100">
+            <span>Neuro Crecimiento</span>
+        </div>
+    )
+}

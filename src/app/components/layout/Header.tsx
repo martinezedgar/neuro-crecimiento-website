8520255cc55d48b9d1e5d6e5ab522bb11f465957
Edgar Martinez <edgar.martinez.tech@gmail.com>
2024-02-07 10:38:41 -0600

    Define the header layout and sticky position

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,7 +1,11 @@
 export const Header = () => {
-    return (
-        <div className="flex justify-center items-center w-[100%] h-[100px] bg-gray-100">
-            <span>Neuro Crecimiento</span>
-        </div>
-    )
+  return (
+      <div className="flex justify-around items-center w-full h-[120px] bg-gray-100 sticky top-0">
+        <div>Logo and Name</div>
+        <p>page 1</p>
+        <p>page 2</p>
+        <p>page 3</p>
+        <div>Button</div>
+      </div>
+  )
 }

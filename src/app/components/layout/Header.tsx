fc08e835756b47cbfb916f01d2b361af97e000f9
Edgar Martinez <edgar.martinez.tech@gmail.com>
2024-02-13 10:21:27 -0600

    Add font family and size to tailwind configuration

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -2,9 +2,9 @@ export const Header = (): JSX.Element => {
   return (
       <div className="flex justify-around items-center w-full h-[120px] bg-gray-100 sticky top-0">
         <div>Logo and Name</div>
-        <p>page 1</p>
-        <p>page 2</p>
-        <p>page 3</p>
+        <p className="font-chivo font-normal text-header-menu">page 1</p>
+        <p className="font-chivo font-normal text-header-menu">page 2</p>
+        <p className="font-chivo font-normal text-header-menu">page 3</p>
         <div>Button</div>
       </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,27 +7,36 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'text': '#101828',
-      'primary': '#508FDA',
-      'secondary': '#8D99AE',
-      'success': '#06D6A0',
-      'danger': '#EF476F',
-      'warning': '#FFD166',
-      'info': '#38A0C2',
-      'bg-beige': '#F2F4F7',
-      'bg-sky-blue': '#1D2939',
-      'bg-blue': '#667085',
-      'bg-green': '#98A2B3',
-      'dark-green': '#006D77',
-      'darkest-green': '#216066',
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'text': '#101828',
+        'primary': '#508FDA',
+        'secondary': '#8D99AE',
+        'success': '#06D6A0',
+        'danger': '#EF476F',
+        'warning': '#FFD166',
+        'info': '#38A0C2',
+        'bg-beige': '#F2F4F7',
+        'bg-sky-blue': '#1D2939',
+        'bg-blue': '#667085',
+        'bg-green': '#98A2B3',
+        'dark-green': '#006D77',
+        'darkest-green': '#216066',
+      },
+      fontFamily: {
+        sans: ['sans-serif'],
+        chivo: ['Chivo']
+      },
+      fontSize: {
+        'body': '14px',
+        'body-lg': '16px',
+        'header-menu': '18px',
+      }
     },
   },
   plugins: [],

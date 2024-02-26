a14dbb79dbac18b04333e7ee2a6aaab48ff5533c
Edgar Martinez <edgar.martinez.tech@gmail.com>
2024-02-26 11:21:32 -0600

    Add footer layout divs

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,6 +1,19 @@
 export const Footer = (): JSX.Element =>  {
     return (
-        <div className="flex w-[100%] h-[300px] bg-darkest-green">
+        <div className="flex flex-col justify-center px-20 w-[100%] h-[250px] bg-darkest-green">
+            <div className="flex">
+                <div>Logo and Name</div>
+            </div>
+            <div className="flex flex-col">
+                <span>Contacto</span>
+                <span>3 Avenida 'A'</span>
+                <span>3-21 Zona 1</span>
+                <span>Escuintla</span>
+            </div>
+            <div className="flex justify-between items-center">
+                <span>CopyRights</span>
+                <div>Redes Sociales</div>
+            </div>
         </div>
     )
 }

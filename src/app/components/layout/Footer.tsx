6fdd6a41b02f3bcf39bdcb615d4d0cb03b6f79ed
Edgar Martinez <edgar.martinez.tech@gmail.com>
2024-02-28 11:51:10 -0600

    Add styles to footer text

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,19 +1,19 @@
 export const Footer = (): JSX.Element =>  {
-    return (
-        <div className="flex flex-col justify-center px-20 w-[100%] h-[250px] bg-darkest-green">
-            <div className="flex">
-                <div>Logo and Name</div>
-            </div>
-            <div className="flex flex-col">
-                <span>Contacto</span>
-                <span>3 Avenida 'A'</span>
-                <span>3-21 Zona 1</span>
-                <span>Escuintla</span>
-            </div>
-            <div className="flex justify-between items-center">
-                <span>CopyRights</span>
-                <div>Redes Sociales</div>
-            </div>
+  return (
+    <div className="flex flex-col justify-center px-20 w-[100%] h-[300px] text-gray-200 font-noto text-[14px] bg-darkest-green">
+        <div className="flex grow-[3] items-center">
+            <div>Logo and Name</div>
         </div>
-    )
+        <div className="flex flex-col justify-center grow-[5]">
+            <span className="font-bold mb-2 font-chivo text-[18px]">Contacto</span>
+            <span>3 Avenida A</span>
+            <span>3-21, Zona 1</span>
+            <span>Escuintla</span>
+        </div>
+        <div className="flex justify-between items-center grow-[2]">
+            <span>Neuro Crecimiento 2024</span>
+            <div>Redes Sociales</div>
+        </div>
+    </div>
+  )
 }
